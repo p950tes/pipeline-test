a25c0db24143c08eef13ede1ee3f425332bf05fa
p950tes <tobiasericsson90@hotmail.com>
2022-10-20 15:01:32 +0200

    Create myPipeline.groovy

---
+++ myPipeline.groovy
@@ -0,0 +1,14 @@
+void call() {
+    echo "my pipeline"
+    pipeline {
+        agent any
+
+        stages {
+            stage ("call github lib") {
+                steps {
+                    helloWorld()
+                }
+            }
+        }
+    }
+}
